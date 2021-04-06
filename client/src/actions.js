d43f17fb5f155d1d28c2b3854c863717cd893b81
MaseDar Ubuntu <mailspecialforspam@mail.ru>
2021-04-06 18:50:08 +0300

    чертовы дяди (а)исины

--- client/src/actions.js
+++ client/src/actions.js
@@ -1,10 +1,9 @@
 // TODO: Решить что делать с разграничением сервисов на 1 домене
-// TODO: Перенести сервер и клиент на разные репозитории 
 // TODO: Сверстать нормальную начальную страницу для каждого пользователя
 // TODO: Попробовать заполнять желания и вытягивать их
-export default function reqTestReq(vk_id, name,firstname) {
+export function reqCreateUser(vk_id, name, firstname) {
     const body = {vk_id, name, firstname}
-        fetch("http://localhost:5000/createuser", {
+        return fetch("http://localhost:5000/createuser", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(body)
@@ -12,7 +11,7 @@ export default function reqTestReq(vk_id, name,firstname) {
         .then(res => res.json())
         .then(
           (result) => {
-            console.log(result);
+            console.log("typeof" ,typeof(result));
             return result;
           },
           (error) => {
@@ -20,4 +19,22 @@ export default function reqTestReq(vk_id, name,firstname) {
             return error;
           }
         )
+}
+export default function reqCreateDesire(vk_id, name, description, genre) {
+  const body = {vk_id, name, description, genre}
+    return fetch("http://localhost:5000/createdesire", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body)
+        })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          return result;
+        },
+        (error) => {
+          console.log(error);
+          return error;
+        }
+      )
 }
--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -1,30 +1,63 @@
-import reqTestReq from "../actions"
-const { React, useState } = require("react");
+import reqCreateDesire from "../actions"
+import {reqCreateUser} from "../actions"
+const { React, useState, useEffect } = require("react");
 const { FormItem, Input, Button, File, Banner, ContentCard} = require("@vkontakte/vkui");
 const {Icon24Document} = require("@vkontakte/icons")
 
 
 function Test (){
-
-    const [vk_id, setVk_id] = useState("1");
+    const [id, setId] = useState([]);
+    const [vk_id, setVk_id] = useState("0");
     const [name, setName] = useState("Влад");
     const [firstname, setFirstname] = useState("Молотков");
+    const [description, setDescription] = useState("ну короче надо около 250к золотых...");
+    const [genre, setGenre] = useState("Годовалый");
 
 
-    const testReq = () => {
-      let result = reqTestReq(vk_id, name, firstname);
-      console.log(result);
-      // setName(result.name);
+    const createUser = () => {
+        let res = [32,8];
+        let result = reqCreateUser(vk_id, name, firstname);
+        console.log(result);
+        console.log("ID:", id);
     };
 
+    // const createDesire = () => {
+    //     let response;
+    //     reqCreateDesire(vk_id, name, description, genre).then(res => {
+    //         response = res;
+    //     })
+    //     .then(() => setId(response))
+    //     .then(() => console.log("И ЭТО...", response));
+    //   };
+
+    const createDesire = async() => {
+        let response;
+        await reqCreateDesire(vk_id, name, description, genre).then(res => {
+            response = res;
+            console.log(response)
+        })
+        //  setId([...response]);
+         console.log("И ЭТО...", response);
+      };
+
     return(
-        <FormItem top="Тестовый стенд">
-            <Input type="text" defaultValue="1" onChange = {e => setVk_id(e.target.value)} />
-            <Input type="text" defaultValue="Влад" onChange = {e => setName(e.target.value)} />
-            <Input type="text" defaultValue="Молотков" onChange = {e => setFirstname(e.target.value)} />
-            <Button onClick={() => testReq()} size="l" stretched style={{ marginRight: 8 }}>Создать</Button>
+        <div>
+        <FormItem top="Создание пользователя">
+            <Input type="text" placeholder="vk_id" onChange = {e => setVk_id(e.target.value)} />
+            <Input type="text" placeholder="name" onChange = {e => setName(e.target.value)} />
+            <Input type="text" placeholder="firstname" onChange = {e => setFirstname(e.target.value)} />
+            <Button onClick={() => createUser()} size="l" stretched style={{ marginRight: 8 }}>Создать</Button>
             Имя индивида: {name}
         </FormItem>
+        <FormItem top="Создание мечты">
+            <Input type="text" placeholder="vk_id (потом автоматом)" onChange = {e => setVk_id(e.target.value)} />
+            <Input type="text" placeholder="Название" onChange = {e => setName(e.target.value)} />
+            <Input type="text" placeholder="Описание" onChange = {e => setDescription(e.target.value)} />
+            <Input type="text" placeholder="Жанр мечты" onChange = {e => setGenre(e.target.value)} />
+            <Button onClick={e  =>  createDesire()} size="l" stretched style={{ marginRight: 8 }}>Создать</Button>
+        </FormItem>
+         {/* {id.map((id) => <p> id from request: {id}</p> )}  */}
+        </div>
 
     )
 }
